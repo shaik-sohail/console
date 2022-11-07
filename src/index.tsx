4e7bb89e929c26819d701087fd596100db941c37
iamkiran27 <saikiran.kavali@zemosolabs.com>
2022-11-07 18:26:55 +0530

    Added theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { StyledEngineProvider, ThemeProvider } from '@mui/material'
 import App from './App'
+import theme from './theme/theme'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
+  </ThemeProvider>,
+
   document.getElementById('root')
 )
---
+++ src/theme/theme.tsx
@@ -0,0 +1,12 @@
+
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+        main: '#2da6ca',
+    },
+  },
+});
+
+
+export default theme;
