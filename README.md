eb0bf43102df098477cda68138435ccd3dbf8523
shaik-sohail <sohail.shaik@zemosolabs.com>
2022-11-07 15:52:50 +0530

    project scaffolding UI Console

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# react-app-without-cra
-# Documentation about this can be found here - https://blog.bitsrc.io/create-react-app-without-create-react-app-b0a5806a92
+# TruValidate Console
--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-const App = () =>{
-    return (
-        <h1>
-            Welcome to React App thats build using Webpack and Babel separately
-        </h1>
-    )
-}
+const App = () => {
+  return <h1>Welcome to TruValidate Console</h1>;
+};
 
-export default App
+export default App;
