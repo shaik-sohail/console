4841ab40d5656c9f5419e99446198cbc8d016f10
shaik-sohail <sohail.shaik@zemosolabs.com>
2022-11-08 21:56:38 +0530

    fix:button custom variants remove

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,41 +1,12 @@
-import { createTheme, Color } from '@mui/material'
-declare module '@mui/material/Button' {
-  interface ButtonPropsVariantOverrides {
-    template: true
-    createTemplate: true
-  }
-}
 
-let theme = createTheme({
+import { createTheme } from '@mui/material/styles';
+const theme = createTheme({
   palette: {
     primary: {
-      main: '#2da6ca',
+        main: '#2da6ca',
     },
   },
-  components: {
-    MuiButton: {
-      variants: [
-        {
-          props: { variant: 'template' },
-          style: {
-            textTransform: 'none',
-            border: `4px solid`,
-            borderRadius: '24px',
-            color: '#2da6ca',
-          },
-        },
-        {
-          props: { variant: 'createTemplate' },
-          style: {
-            textTransform: 'none',
-            borderRadius: '24px',
-            backgroundColor: '#2da6ca',
-            color: '#fff',
-          },
-        },
-      ],
-    },
-  },
-})
+});
 
-export default theme
+
+export default theme;
