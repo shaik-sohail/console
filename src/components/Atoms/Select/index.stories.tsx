39d87959e1c33711e26c6a5884edcce00e5a4fed
shaik-sohail <sohail.shaik@zemosolabs.com>
2022-11-11 11:37:57 +0530

    feat: add story

---
+++ src/components/Atoms/Select/index.stories.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Select from '.'
+import { Divider, Grid, Typography } from '@mui/material'
+
+export default {
+  title: 'Atoms/SearchField',
+  component: Select,
+  argTypes: {},
+} as ComponentMeta<typeof Select>
+
+const Template: ComponentStory<typeof Select> = (args) => (
+  <div style={{ width: '250px' }}>
+    <Select {...args} />
+  </div>
+)
+
+function MItem() {
+  return (
+    <Grid container sx={{ width: '150px' }}>
+      <Grid item container justifyContent={'space-between'}>
+        <Grid item>
+          <Typography>Sohail</Typography>
+        </Grid>
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{ border: '1px solid grey' }}
+        />
+        <Grid item>
+          <Typography>Zemoso</Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+export const PrimarySelect = Template.bind({})
+PrimarySelect.args = {
+  value: '1',
+  items: [{ id: '1', content: <MItem /> }],
+}
